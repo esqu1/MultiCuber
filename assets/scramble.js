1190826b4a7097f4c46fcadf3c09558ce950e5cc
esqu1 <brandbest1@gmail.com>
2017-06-05 11:19:26 -0400

    moved location of jsss

--- assets/scramble.js
+++ assets/scramble.js
@@ -1,12 +1,15 @@
-var scramble_333 = require('./jsss/scramble_333.js');
-var scramble_sq1 = require('./jsss/scramble_sq1.js');
-var scramble_222 = require('./jsss/scramble_222.js');
-var scramble_NNN = require('./jsss/scramble_NNN.js');
-var scramble_minx = require('./jsss/scramble_minx.js');
-var scramble_pyram = require('./jsss/scramble_pyram.js');
-var scramble_clock = require('./jsss/scramble_clock.js');
+var scramble_333 = require('../public/jsss/scramble_333.js');
+var scramble_sq1 = require('../public/jsss/scramble_sq1.js');
+var scramble_222 = require('../public/jsss/scramble_222.js');
+var scramble_NNN = require('../public/jsss/scramble_NNN.js');
+var scramble_minx = require('../public/jsss/scramble_minx.js');
+var scramble_pyram = require('../public/jsss/scramble_pyram.js');
+var scramble_clock = require('../public/jsss/scramble_clock.js');
 
 const scramblers = Object.assign({}, scramble_333.scramblers, scramble_sq1.scramblers, scramble_222.scramblers, scramble_NNN.scramblers, scramble_minx.scramblers,
 	scramble_pyram.scramblers, scramble_clock.scramblers);
 
-console.log(scramblers)
+
+var scramble333 = (callback) => {
+	callback(scramblers['333'].getRandomScramble().scramble_string);
+}
